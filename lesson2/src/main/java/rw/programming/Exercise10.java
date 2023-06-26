fbdecb6a4145378311b8de9f1292c88ee9464d39
Sergio Navarrete <sergio.nava89@gmail.com>
2023-06-26 19:32:53 +0100

    more exercises

---
+++ lesson2/src/main/java/rw/programming/Exercise10.java
@@ -0,0 +1,21 @@
+package rw.programming;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Exercise10 {
+
+  public static void main(String[] args) {
+    System.out.println("Enter your date of birth in the format YYYY-mm-dd");
+    Scanner sc = new Scanner(System.in);
+    String input = sc.nextLine();
+
+    LocalDate dateOfBirth = null; // Replace with the code to transform input into a date using the parse function.
+    LocalDate now = null; // Replace with the code to get now date.
+
+    // Complete the method to calculate the age.
+    // Use methods getYear and getDayOfTheYear from the date
+    // ex. dateOfBirth.getYear()  dateOfBirth.getDayOfTheYear()
+  }
+
+}
